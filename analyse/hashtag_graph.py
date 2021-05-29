4af6aaafc64dacab99bb43669cd5ab14410023c2
Nattaphum Saito <nattaphum.sa@ku.th>
2021-05-29 18:52:45 +0700

    create hashtag graph

---
+++ analyse/hashtag_graph.py
@@ -0,0 +1,55 @@
+import os
+from itertools import product
+
+from pymongo.database import Database
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import networkx as nx
+
+load_dotenv('../env/mongo_auth.env')
+
+def connect() -> Database:
+    client = MongoClient(
+        host=os.getenv('HOST'),
+        username=os.getenv('USERNAME'),
+        password=os.getenv('PASSWORD'),
+        authSource=os.getenv('AUTHSOURCE'),
+        authMechanism=os.getenv('AUTHMECH')
+    )
+
+    db = client.get_database( os.getenv('AUTHSOURCE') )
+    return db
+
+def create_graph( step : int ) -> nx.Graph:
+    roots = { 'ReviewThailand', 'unseenthailand', 'amazingthailand', 'TourismAuthorityOfThailand' }
+
+    db = connect()
+
+    next_hop = roots
+    visited = set()
+
+    G = nx.graph()
+
+    for i in range(step):
+        edges = set()
+
+        records = db.get_collection( 'tweets' ).find({
+            'hashtags' : { '$in' : list(next_hop) }
+        })
+
+        visited = visited.union( next_hop )
+        next_hop.clear()
+
+        for record in records:
+            next_hop = next_hop.union( record['hashtags'] )
+            edges = edges.union( product( record['hashtags'], record['hashtags'] ) )
+        
+        next_hop.difference( visited )
+
+        G.add_edges_from( list(edges) )
+
+    return G
+
+if __name__ == '__main__':
+    G = create_graph( 10 )
+    nx.write_adjlist( G, 'graph.adjlist' )
